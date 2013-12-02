b5d1c6f9135f125012fd0adfa4de0cd35b9a84bd
Brian Garrett <popavo@gmail.com>
2013-12-02 13:33:42 -0600

    Added static library target and set GBCli executable to link with libgbcli

---
+++ libgbcli/libgbcli.h
@@ -0,0 +1,13 @@
+//
+//  libgbcli.h
+//  libgbcli
+//
+//  Created by Brian K Garrett on 12/2/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import "GBCommandLineParser.h"
+#import "GBOptionsHelper.h"
+#import "GBSettings.h"
---
+++ libgbcli/libgbcli.m
@@ -0,0 +1,9 @@
+//
+//  libgbcli.m
+//  libgbcli
+//
+//  Created by Brian K Garrett on 12/2/13.
+//
+//
+
+#import "libgbcli.h"
